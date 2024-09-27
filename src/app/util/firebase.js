2fc79fe4274b7d31ba28be50ed33a94d3410ccf2
Ashley Corona <ashleeglasss@gmail.com>
2024-09-27 16:45:36 -0400

    firebase enviro update

--- src/app/util/firebase.js
+++ src/app/util/firebase.js
@@ -3,19 +3,20 @@ import { getFirestore } from "firebase/firestore";
 import { isSupported, getAnalytics } from "firebase/analytics"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJPkpN__JOlnZup74pBKfe9RL37ieG9B8",
-  authDomain: "xbrainstewx.firebaseapp.com",
-  projectId: "xbrainstewx",
-  storageBucket: "xbrainstewx.appspot.com",
-  messagingSenderId: "770515421710",
-  appId: "1:770515421710:web:86b7fff216d91942064a5f",
-  measurementId: "G-73PTWKTDQ2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Analytics is optional; wrap it in isSupported
 isSupported().then((supported) => {
   if (supported) {
     const analytics = getAnalytics(app);
